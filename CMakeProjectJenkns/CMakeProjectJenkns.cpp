8af5d93e0363820bd8ae59f5280a63e44aef9522
UmadeviGit <94173120+UmadeviGit@users.noreply.github.com>
2022-02-24 13:47:50 +0530

    Update CMakeProjectJenkns.cpp

--- CMakeProjectJenkns/CMakeProjectJenkns.cpp
+++ CMakeProjectJenkns/CMakeProjectJenkns.cpp
@@ -10,6 +10,8 @@ int main()
 	cout << "Hello CMake." << endl;
 	cout << "commit initited." << endl;
 	cout << "commit 2 initited." << endl;
+	cout << "commit 3 initited." << endl;
+
 
 	return 0;
 }
